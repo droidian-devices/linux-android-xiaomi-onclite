389a136c8e58e48e97703b74c61f31274b492ed8
Bardia Moshiri <fakeshell@bardia.tech>
2023-02-14 15:52:11 +0330

    specify the dtb don't fallback to auto search

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -48,7 +48,7 @@ KERNEL_IMAGE_WITH_DTB = 1
 # Path to the DTB
 # If you leave this undefined, an attempt to find it automatically
 # will be made.
-#KERNEL_IMAGE_DTB = arch/arm64/boot/dts/qcom/my_dtb.dtb
+KERNEL_IMAGE_DTB = arch/arm64/boot/dts/qcom/sdm632.dtb
 
 # Whether to include a DTB Overlay. Use 0 (no) or 1.
 KERNEL_IMAGE_WITH_DTB_OVERLAY = 1
